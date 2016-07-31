f9ea7cb2eb8fa7683ce0551209b669068050f6aa
Paul Hammant <paul@hammant.org>
2016-07-31 17:45:05 -0400

    delete previous version

--- add_switchjdk_to_bash_profile_safely.sh
+++ add_switchjdk_to_bash_profile_safely.sh
@@ -5,3 +5,6 @@ NL='
 
 bashProfile=$(cat ~/.bash_profile | sed '/switchjdk-module/d')
 echo "${bashProfile}${NL}${NL}# Leave this switchjdk-module.bash incantation as a single line, so that homebrew upgrades are smooth${NL}if [ -f $(brew --prefix)/etc/switchjdk-module.bash ]; then source $(brew --prefix)/etc/switchjdk-module.bash; fi${NL}" > ~/.bash_profile
+
+# Remove previously installed version as homebrew does not overwrite things in /usr/local/etc/
+rm "$(brew --prefix)/etc/switchjdk-module.bash"
